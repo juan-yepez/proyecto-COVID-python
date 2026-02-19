3ee76fa07d3fa5f990d5f571057ba983ee849d04
juan-yepez <juan.yepez@utp.edu.co>
2026-02-19 14:37:47 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
+from api.consulta import obtener_datos
+from ui.interfaz import pedir_datos, mostrar_tabla
 
+def inicio():
+    departamento, limite = pedir_datos()
+    datos = obtener_datos(departamento, limite)
+    mostrar_tabla(datos)
+
+if __name__ == "__main__":
+    inicio()
